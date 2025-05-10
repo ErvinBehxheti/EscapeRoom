6ad98ccd97a41e19d3b5c35ab6de553d2ce2f7b5
ErvinBehxheti <ervinbehxheti05@gmail.com>
2025-05-10 14:20:03 +0200

    monkey data

---
+++ evidencaMonkey.js
@@ -0,0 +1,104 @@
+[
+    {
+        "name": "Ervin Behxheti",
+        "age": 20,
+        "WPM": 91,
+        "Accuracy": 93 
+    },
+    {
+        "name": "Olti Jusufi",
+        "age": 14,
+        "WPM": 44,
+        "Accuracy": 78 
+    },
+    {
+        "name": "Edonart Ademi",
+        "age": 16,
+        "WPM": 25,
+        "Accuracy": 98 
+    },
+    {
+        "name": "Enes Abdurrahmani",
+        "age": 13,
+        "WPM": 54,
+        "Accuracy": 89 
+    },
+    {
+        "name": "Atik Kadriu",
+        "age": 13,
+        "WPM": 70,
+        "Accuracy": 81 
+    },
+    {
+        "name": "Edison Ahmeti",
+        "age": 17,
+        "WPM": 66,
+        "Accuracy": 90 
+    },
+        {
+        "name": "Ervin Behxheti",
+        "age": 20,
+        "WPM": 91,
+        "Accuracy": 93 
+    },
+    {
+        "name": "Erduan Fejza",
+        "age": 17,
+        "WPM": 29,
+        "Accuracy": 87 
+    },
+    {
+        "name": "Dasara Beqiri",
+        "age": 13,
+        "WPM": 44,
+        "Accuracy": 96
+    },
+    {
+        "name": "Tuana Beqiri",
+        "age": 13,
+        "WPM": 24,
+        "Accuracy": 91 
+    },
+    {
+        "name": "Olsa Bislimi",
+        "age": 13,
+        "WPM": 45,
+        "Accuracy": 89
+    },
+    {
+        "name": "Eris Peci",
+        "age": 19,
+        "WPM": 53,
+        "Accuracy": 95 
+    },
+    {
+        "name": "Eren Nuka",
+        "age": 13,
+        "WPM": 104,
+        "Accuracy": 99 
+    },
+    {
+        "name": "Sahit Pllana",
+        "age": 13,
+        "WPM": 44,
+        "Accuracy": 92 
+    },
+    {
+        "name": "Ari Fazliu",
+        "age": 13,
+        "WPM": 71,
+        "Accuracy": 92
+    },
+    {
+        "name": "Ermal Qela",
+        "age": 13,
+        "WPM": 29,
+        "Accuracy": 89 
+    },
+    {
+        "name": "Gentrit Sylejmani",
+        "age": 13,
+        "WPM": 34,
+        "Accuracy": 97 
+    }
+]
